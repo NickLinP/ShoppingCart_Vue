0a73192a7a8fb87d3e025bfa3ed3cd538a5ee6e2
NickLinP <101267679+NickLinP@users.noreply.github.com>
2022-10-25 14:17:10 +0800

    購物車sticky修正

---
+++ copy/1025_購物車sticky修正/html/homepage.vue
@@ -0,0 +1,327 @@
+<template>
+    <div class="">
+      <!-- banner拖曳切換 slick -->
+      <div class="row">
+        <div class="col-xl-2"></div>
+        <div class="col-lg-12 col-xl-8">
+          <section class="single-item">
+            <div>
+              <img src="./image/homepage/kashima.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/689ab2cc6e05c466ece2f3ff29d8ea42.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/77c3e3f01e89cb473a104ae0a87b068c.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/95badf6a1ee8f1560f042cee637685d0.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/d28ea12af302a26a85c7a8170fe99040.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/1268c4296fb79fea448316b932cadfd1.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/674c6c827a9af1ecd7e3b86f866571fd.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/3ded5f0c807d6e78f903d60ae3a33949.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/f86ebbc5f9bf8892adfd275d6d892020_compressed_1.jpg" />
+            </div>
+          </section>
+        </div>
+        <div class="col-lg-12"></div>
+      </div>
+
+      <!-- slick 小圖拖曳 輪播 -->
+      <div class="row">
+        <div class="col-xl-2"></div>
+        <div class="col-lg-12 col-xl-8">
+          <section class="slideImg">
+            <div>
+              <img src="./image/homepage/b78cdfb667aaa564e70a28326d8ab67c.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/ebc601b36dd29b99c3861d1ecab8d346.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/2e1bf1373df59e467c961ec8f321ae0e.jpg" />
+            </div>
+            <div>
+              <img src="./image/homepage/01cd935a2485cdb4d4c3dd017262b55b.jpg" />
+            </div>
+          </section>
+        </div>
+        <div class="col-xl-12"></div>
+      </div>
+
+      <!-- 熱賣商品 -->
+      <div class="row">
+        <div class="col-xl-2"></div>
+        <div class="col-lg-12 col-xl-8">
+          <h5 class="d-md-none">熱賣商品</h5>
+          <h3 class="d-none d-md-block">熱賣商品</h3>
+          <div class="hotItem d-flex align-content-start flex-wrap justify-content-between" id="hotItem">
+            <div>
+              <!-- <a href="./product.html" v-bind:id="hotItem[0].id">
+                <img v-bind:src="hotItem[0].src" />
+              </a> -->
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[0].id } }">
+                <img v-bind:src="hotItem[0].src" />
+              </router-link>
+              <p>{{ hotItem[0].name }}</p>
+              <p>NT{{ hotItem[0].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[1].id } }">
+                <img v-bind:src="hotItem[1].src" />
+              </router-link>
+              <p>{{ hotItem[1].name }}</p>
+              <p>NT{{ hotItem[1].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[2].id } }">
+                <img v-bind:src="hotItem[2].src" />
+              </router-link>
+              <p>{{ hotItem[2].name }}</p>
+              <p>NT{{ hotItem[2].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[3].id } }">
+                <img v-bind:src="hotItem[3].src" />
+              </router-link>
+              <p>{{ hotItem[3].name }}</p>
+              <p>NT{{ hotItem[3].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[4].id } }">
+                <img v-bind:src="hotItem[4].src" />
+              </router-link>
+              <p>{{ hotItem[4].name }}</p>
+              <p>NT{{ hotItem[4].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[5].id } }">
+                <img v-bind:src="hotItem[5].src" />
+              </router-link>
+              <p>{{ hotItem[5].name }}</p>
+              <p>NT{{ hotItem[5].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[6].id } }">
+                <img v-bind:src="hotItem[6].src" />
+              </router-link>
+              <p>{{ hotItem[6].name }}</p>
+              <p>NT{{ hotItem[6].price }}</p>
+            </div>
+
+            <div>
+              <router-link :to="{ path: 'product', query: { productNum: hotItem[7].id } }">
+                <img v-bind:src="hotItem[7].src" />
+              </router-link>
+              <p>{{ hotItem[7].name }}</p>
+              <p>NT{{ hotItem[7].price }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-2"></div>
+      </div>
+    </div>
+
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      hotItem: [
+        {
+          id: "11",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+        {
+          id: "",
+          name: "",
+          src: "",
+          price: "",
+        },
+      ],
+      count: 0,
+    };
+  },
+  methods: {
+  },
+  created() {
+
+    console.log(`homepage created`);
+    // 必須先透過暫存區將hotItem解構出來，在下面axios才有辦法將值傳遞回去
+    let objTemp = this.hotItem;
+
+    // 訪問data內的變數使用this是正確的
+    // console.log(this.hotItem[0].id);
+    const url = "./json/hotItem.json";
+    axios
+      .get(`${url}`)
+      .then(function (res) {
+        console.log(url); 
+        for (let i = 0; i <= 7; i++) {
+          // 這個地方無法使用this訪問到data內的變數，因為是在一個function內， this會訪問到其他物件
+          // console.log(this.hotItem[0].id);
+
+          objTemp[i].id = res.data[i].productNum;
+          objTemp[i].name = res.data[i].name;
+          objTemp[i].src = res.data[i].imageMain;
+          objTemp[i].price = res.data[i].price;
+        }
+      })
+      .catch(function (error) {
+        console.log(url);
+        console.log(error);
+      });
+  },
+  mounted() {
+    // <!-- slick 小圖拖曳 輪播 -->
+
+    $('.slideImg').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false
+    });
+
+    $('.single-item').slick({
+      dots: true,
+      arrows: false
+    });
+  },
+};
+</script>
+
+<style scope>
+/* <!-- 整體環境 --> */
+
+.row {
+  margin: 0;
+  padding: 0;
+}
+
+/* 物件置中 */
+.middle {
+  margin: 0 auto;
+}
+
+
+/* <!--slick小圖拖曳輪播--> */
+* {
+  box-sizing: border-box;
+}
+
+.slick-slide {
+  margin-top: 10px;
+  padding: 0 5px 0 5px;
+}
+
+.slick-slide img {
+  width: 100%;
+}
+
+/* <!-- 熱賣商品 --> */
+@media (min-width: 767px) {
+  .hotItem div {
+    width: 300px;
+    height: 250px;
+    margin: 0 0 100px 0;
+    /* border: 1px solid green; */
+    /* background-color: lightblue; */
+  }
+
+  .hotItem p {
+    text-align: center;
+    width: 70%;
+    font-size: 16px;
+    /* border: 1px solid red; */
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 767px) {
+  .hotItem div {
+    width: 150px;
+    height: 125px;
+    margin: 0 0 90px 0;
+    /* border: 1px solid green; */
+    /* background-color: lightblue; */
+  }
+
+  .hotItem p {
+    text-align: center;
+    width: 70%;
+    font-size: 8px;
+    /* border: 1px solid red; */
+    margin: 0 auto;
+  }
+}
+
+.hotItem img {
+  /* border: 1px solid gray; */
+  display: block;
+  margin: 0 auto;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+}
+
+.hotItem p:nth-child(3) {
+  color: red;
+}
+</style>
---
+++ copy/1025_購物車sticky修正/html/product.vue
@@ -0,0 +1,249 @@
+<template>
+  <div>
+    <!-- 分類標籤 -->
+    <div class="text-center listLabel">
+      <p>
+        <a href="">首頁</a>
+        <span>></span>
+        <a href="">寵物玩具</a>
+      </p>
+    </div>
+
+    <div class="row my-5">
+      <div class="col-xl-2"></div>
+      <!-- 商品圖片 -->
+      <div class="col-xl-4 col-12">
+        <img id="mainImg" v-bind:src="info.imageMain" class="w-100 img-thumbnail d-block" />
+        <div class="my-3">
+          <img id="itemThumbnail0" v-bind:src="info.imageMain" class="imgChange w-25 img-thumbnail" />
+          <img id="itemThumbnail1" v-bind:src="info.imageSecondary" class="imgChange w-25 img-thumbnail" />
+        </div>
+      </div>
+
+      <div class="col-xl-4 col-12">
+        <div>
+          <p class="h1">{{ info.name }}</p>
+        </div>
+
+        <div class="mx-3">
+          <p>
+            <span style="color: #8e44ad">{{ info.description[0] }}</span>
+            <br />
+            <span style="color: #6633ff">{{ info.description[1] }}</span>
+            <br />
+            <span style="color: #ff33ff">{{ info.description[2] }}</span>
+            <br />
+            <span style="color: #e67e22">{{ info.description[3] }}</span>
+            <br />
+            <span style="color: #2980b9">{{ info.description[4] }}</span>
+          </p>
+        </div>
+
+        <hr />
+
+        <div class="mx-5">
+          <p>付款方式：信用卡、貨到付款、超商取貨付款</p>
+          <p>運送方式：</p>
+          <ul>
+            <li>信用卡-宅配/貨運 - 單件最高運費90元</li>
+            <li>貨到付款-宅配/貨運 - 單件最高運費120元</li>
+            <li>超商取貨付款 - 單件運費80元</li>
+            <li>台灣外島 - 單件運費180元</li>
+          </ul>
+          <span>如何更便宜?→查看運費說明</span>
+        </div>
+
+        <div>
+          <p class="h2 my-3 text-danger">NT {{ info.price }}</p>
+        </div>
+
+        <hr />
+
+        <!-- 商品數量增加減少 -->
+        <div class="quantityControl input-group bootstrap-touchspin w-50">
+          <button v-on:click="reduceQuantity()" id="quantitySub" class="btn border">
+            -
+          </button>
+          <input type="number" v-model="info.quantity" name="" id="productQuantity"
+            class="text-center form-control inline-block" />
+          <button v-on:click="addQuantity()" id="quantityAdd" class="btn border">
+            +
+          </button>
+        </div>
+
+        <div>
+          <br />
+          <button class="bg-danger text-light border-0 p-2">直接購買</button>
+          <button v-on:click="addShopCart()" class="bg-primary text-light border-0 p-2">
+            加入購物車
+          </button>
+        </div>
+      </div>
+      <div class="col-xl-2"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return {
+        // info的內容在畫面載入時會由axios取的的商品資料進行複寫
+        info: {
+          name: "123",
+          description: [],
+          price: "999",
+          quantity: 1,
+          imageMain: "",
+          imageSecondary: "",
+          productNum: this.$route.query.productNum,
+        },
+        pressAddShopCartCount: 0,
+
+      };
+    },
+    methods: {
+      addShopCart() {
+        // 將產品圖片、名稱、數量、價格推到陣列內，等待顯示用
+        var shopCartBuffer;
+        if (Cookies.get("shopCartMemory") === undefined) {
+          // 如果購物車是空的就先寫入空陣列，後續才用push帶入資料，這樣在Cookie內保存內容資料才不會出錯
+          shopCartBuffer = []
+          shopCartBuffer.push([
+            this.info.imageMain,
+            this.info.name,
+            this.info.quantity,
+            this.info.price,
+          ]);
+        } else {
+          shopCartBuffer = JSON.parse(Cookies.get("shopCartMemory"));
+          shopCartBuffer.push([
+            this.info.imageMain,
+            this.info.name,
+            this.info.quantity,
+            this.info.price,
+          ]);
+        }
+
+        // 按下加入購物車後送出訊號給外層進一步觸發更新機制
+        console.log('商品頁面內按下加入購物車');
+        this.pressAddShopCartCount += 1;
+        this.$emit('update-shop-cart', this.pressAddShopCartCount);
+
+        // console.log(`購物車新增前的內容 : ${this.shopCartBuffer}`);
+
+        // console.log(`購物車新增後的內容 : ${shopCartBuffer}`);
+
+        // 將加入購物車的商品推到Cookies內進行保存，並且header也可以讀取到
+        Cookies.set("shopCartMemory", JSON.stringify(shopCartBuffer));
+        console.log(`現在購物車內容是 : ${Cookies.get("shopCartMemory")}`);
+      },
+
+      // 商品數量修改　↓
+      addQuantity() {
+        if (this.info.quantity < 100) {
+          this.info.quantity = Number(this.info.quantity) + 1;
+          // 以下這行會出錯，如果用鍵盤輸入了一個數字，會被判別為字串型別
+          // 例如輸入50在按加號，會在畫面顯示501而非51
+          // this.info.quantity += 1;
+
+          // console.log(`目前數量是${this.info.quantity}`);
+          // console.log(`該商品金額是${this.info.price}`);
+        }
+      },
+      reduceQuantity() {
+        if (this.info.quantity > 1) {
+          this.info.quantity = Number(this.info.quantity) - 1;
+          // this.info.quantity -= 1;
+
+          // console.log(`目前數量是${this.info.quantity}`);
+          // console.log(`該商品金額是${this.info.price}`);
+        }
+        // 商品數量修改　↑
+      },
+
+    },
+    // beforeCreate() {
+    //   // 將Cookie的購物車紀錄帶入data內等待使用
+    //   if (Cookies.get("shopCartMemory") !== undefined) {
+    //     this.shopCartBuffer = JSON.parse(Cookies.get("shopCartMemory"));
+    //     console.log(`cookie購物車記錄是 : ${this.shopCartBuffer}`);
+    //   }else{
+    //     console.log(`cookie購物車紀錄為空`);  
+    //   }
+
+    // },
+
+    created() {
+      console.log("created");
+
+      var objTemp = this.info;
+      console.log(objTemp);
+      // console.log(this.productNum)
+      // 訪問data內的變數使用this是正確的
+      // console.log(this.hotItem[0].id);
+      const url = "./json/hotItem.json";
+      axios
+        .get(`${url}`)
+        .then(function (res) {
+          console.log(`axios成功:${url}`);
+          objTemp.name = res.data[objTemp.productNum].name;
+          objTemp.description = res.data[objTemp.productNum].description;
+          objTemp.imageMain = res.data[objTemp.productNum].imageMain;
+          objTemp.imageSecondary = res.data[objTemp.productNum].imageSecondary;
+          objTemp.price = res.data[objTemp.productNum].price;
+        })
+        .catch(function (error) {
+          console.log(`axios失敗:${url}`);
+          console.log(error);
+        });
+    },
+    mounted() {
+      // <!-- 商品圖片切換 -->
+
+      $(document).ready(function () {
+        console.log("圖片切換");
+        $(".imgChange").on("mouseover", function () {
+          // console.log(this);
+          var img = $("#mainImg").attr("src", $(this).attr("src"));
+          // console.log(img);
+        });
+      });
+    },
+  };
+</script>
+
+<style scoped>
+  /* <!-- 整體環境 --> */
+
+  /* body {
+            background-color: aqua;   
+        } */
+
+  .row {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* 物件置中 */
+  .middle {
+    margin: 0 auto;
+  }
+
+  /* <!-- 分類標籤 --> */
+
+  .listLabel a {
+    text-decoration: none;
+    color: gray;
+  }
+
+  /* 去除input預設的上下鍵 */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+</style>
